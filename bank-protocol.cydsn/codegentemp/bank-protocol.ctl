b0cbbfc6ced13abd7217b70e0758c06b64c8d1bd
Larry Geng <larry.geng12@gmail.com>
2018-07-16 10:43:44 -0400

    build 2

--- bank-protocol.cydsn/codegentemp/bank-protocol.ctl
+++ bank-protocol.cydsn/codegentemp/bank-protocol.ctl
@@ -1,6 +1,6 @@
 -- ======================================================================
 -- bank-protocol.ctl generated from bank-protocol
--- 07/16/2018 at 10:39
+-- 07/16/2018 at 10:42
 -- This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
 -- ======================================================================
 
--- bank-protocol.cydsn/codegentemp/bank-protocol.v
+++ bank-protocol.cydsn/codegentemp/bank-protocol.v
@@ -1,6 +1,6 @@
 // ======================================================================
 // bank-protocol.v generated from TopDesign.cysch
-// 07/16/2018 at 10:39
+// 07/16/2018 at 10:42
 // This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
 // ======================================================================
 
