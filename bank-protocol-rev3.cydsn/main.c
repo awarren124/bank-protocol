122d583a7a7b4ed537915cf67f16b89bfb302f42
Larry Geng <larry.geng12@gmail.com>
2018-07-18 22:37:29 -0400

    readded uart to rev3

--- bank-protocol-rev3.cydsn/main.c
+++ bank-protocol-rev3.cydsn/main.c
@@ -11,12 +11,30 @@
 */
 #include <project.h>
 #include <mbedtls/rsa.h>
+#include <mbedtls/pk.h>
 
 int main (void)
 {
     CyGlobalIntEnable;      /* Enable global interrupts */
     
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    
+    UART_Start();
+    UART_PutString("hello world\n");
+    
+    //RSA
+    int ret = 0;
+    
+    mbedtls_pk_context pk;
+    mbedtls_pk_init( &pk );
+    
+    unsigned char buf[MBEDTLS_MPI_MAX_SIZE];
+    size_t olen = 0;
+    
+    //Card information
+    
+    long long exampleCreditCardNumber = 4753245442839561;
+    char * exampleExpirationDate = "0922";
     
     for(;;)
     {
