c7696db9b859f5417dbdaf2e85b02ca3becd7910
unknown <technoal9@gmail.com>
2018-07-23 11:21:51 -0400

    fixed error in the database default code

--- bank/db.py
+++ bank/db.py
@@ -24,6 +24,8 @@ class DB(object):
         return os.path.exists(self.path)
 
     def modify(self, table, k, subks, vs):
+        if not self.exists():
+            self.init_db()
         with open(self.path, 'r') as f:
             db = json.loads(f.read())
 
