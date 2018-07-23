7802b5981f95326f8079cb3c3e00d9dc6a8f1af3
P250-SandDune <lib@bxscience.edu>
2018-07-23 10:12:19 -0400

    example CARD-rev3

--- CARD-rev3.cydsn/cyapicallbacks.h
+++ CARD-rev3.cydsn/cyapicallbacks.h
@@ -12,10 +12,8 @@
 #ifndef CYAPICALLBACKS_H
 #define CYAPICALLBACKS_H
     
-
     /*Define your macro callbacks here */
-    /*For more information, refer to the Writing Code topic in the PSoC Creator Help.*/
-
+    /*For more information, refer to the Macro Callbacks topic in the PSoC Creator Help.*/
     
 #endif /* CYAPICALLBACKS_H */   
 /* [] */
