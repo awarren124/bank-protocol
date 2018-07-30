28e2c89086cd5b1fb335bc76665176ef17a777e9
P250-SandDune <lib@bxscience.edu>
2018-07-30 11:21:45 -0400

    test code for aes on PSoC

--- CARD-rev5.cydsn/main.c
+++ CARD-rev5.cydsn/main.c
@@ -87,23 +87,41 @@ int main (void)
     CyGlobalIntEnable;      /* Enable global interrupts */
     
     UART_Start();
-    
-    /* Declare variables here */
     uint8 message[128];
+    /*printUART("Hey, PSoC1\t", 11);
     
-    /*
-    printUART("hello world\n", 13);
-    char * received = recvUART(8);
-    printUART(received, 8);
-    free(received);
-    */
-    cf_aes_context aes_ctx;
+    
+    
+    uint8_t out[16];
+    const void *iv =  "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f";
+    const void *key = "\x2b\x7e\x15\x16\x28\xae\xd2\xa6\xab\xf7\x15\x88\x09\xcf\x4f\x3c";
+    const void *inp = "\x6b\xc1\xbe\xe2\x2e\x40\x9f\x96\xe9\x3d\x7e\x11\x73\x93\x17\x2a";
+    const void *expect = "\x76\x49\xab\xac\x81\x19\xb2\x46\xce\xe9\x8e\x9b\x12\xe9\x19\x7d";
+
+    cf_aes_context aes;
+    cf_aes_init(&aes, key, 16);
+
+    cf_cbc cbc;
+    cf_cbc_init(&cbc, &cf_aes, &aes, iv);
+    cf_cbc_encrypt(&cbc, inp, out, 1);
+    printUART(out, 16);
+    printUART((char *)out, 16);
+    UART_PutArray(out, 16);
+    printUART("HeyPSoC2\t", 11);
+    
+    
+    
+    uint8 message[128];*/
+    /* Declare variables here */
+
+    /*cf_aes_context aes_ctx;
     cf_cbc cbc_ctx;
-    uint8_t outbuf[16], tmp[16];
+    uint8_t outbuf[16];
     
-    const uint8_t key1[] = {0xe6, 'R', '|', 0x84,'x', 0xce, 0x96, 0xa5, 'T',0xac,0xd8,'l',0xd0,0xe4,'L','f',0xf6,'&',0x16,'E',0xfa,'/',0x9b,0xa2,0xea,'!',0xce,'Y',0x85,0xbe,'\\','r','a'};
-    uint8_t iv[13] = "this is an iv";
-    uint8_t inbuf[] = {'a', 's', 'd', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f'};
+    //const uint8_t *key1 = {0xe6, 'R', '|', 0x84,'x', 0xce, 0x96, 0xa5, 'T',0xac,0xd8,'l',0xd0,0xe4,'L','f',0xf6,'&',0x16,'E',0xfa,'/',0x9b,0xa2,0xea,'!',0xce,'Y',0x85,0xbe,'\\','r','a'};
+    const void *key1 = "\xe6" "R" "\x84" "x" "\xce" "\x96" "\xa5" "T" "\xac\xd8" "l" "\xd0\xe4" "Lf" "\xf6" "&" "\x16" "E" "\xfa" "/" "\x9b\xa2\xea" "!" "\xce" "Y" "\x85\xbe" "\\r";
+    const void *iv = "this is an iv";
+    const void *inbuf = "facefeed";
     uint8_t cbuf[] = {0x0e, 'p', 0xbd, 'l',0xa0, 'D','6',0x0b,0x00,'\\','W',0x87,0xca,'\\'};
     
     cf_aes_init(&aes_ctx, key1, 32);
@@ -111,11 +129,14 @@ int main (void)
     
     cf_cbc_encrypt(&cbc_ctx, inbuf, outbuf, 32);
     
+    printUART("Hey, PSoC2\t", 11);
+    
     UART_PutArray(outbuf, 16);
+    
     
     cf_cbc_decrypt(&cbc_ctx, cbuf, outbuf,32);
     
-    printUART(outbuf, 16);
+    printUART(outbuf, 16); */
     
     // Provision card if on first boot
     if (*PROVISIONED == 0x00) {
