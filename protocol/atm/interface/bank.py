8d628f68a7721c5c684df4ec4ed6f8cb86b1b45e
Alexander Warren <arocks124@gmail.com>
2018-07-31 09:44:58 -0400

    updated interface/bank.py

---
+++ protocol/atm/interface/bank.py
@@ -0,0 +1,125 @@
+"""Backend of ATM interface for xmlrpc"""
+
+import logging
+import struct
+import serial
+from encryptionHandler import EncryptionHandler
+eh = EncryptionHandler()
+
+
+"""TODO: MAKE KEYS STORED IN A JSON FILE"""
+"""TEMPORARRRYYYYY"""
+key1 = b'\xe6R|\x84x\xce\x96\xa5T\xac\xd8l\xd0\xe4Lf\xf6&\x16E\xfa/\x9b\xa2\xea!\xceY\x85\xbe\ra'
+key2 = b'\xb5\xd2\x03v\xad)\xd5\x8a \xa6\xa0_\x94^\xe6X=$&|&\xd4c*#M\xee[\tl\xfc\xd0'
+
+"""~~~~~~~~~~~~~~~~~"""
+
+
+
+
+class Bank:
+    """Interface for communicating with the bank
+
+    Args:
+        port (serial.Serial): Port to connect to
+    """
+
+    def __init__(self, port, verbose=False):
+        self.ser = serial.Serial(port, baudrate = 115200)
+        self.verbose = verbose
+
+    def _vp(self, msg, stream=logging.info):
+        """Prints message if verbose was set
+
+        Args:
+            msg (str): message to print
+            stream (logging function, optional): logging function to call
+        """
+        if self.verbose:
+            stream("card: " + msg)
+
+    def check_balance(self, atm_id, card_id):
+        """Requests the balance of the account associated with the card_id
+
+        Args:
+            atm_id (str): UUID of the ATM
+            card_id (str): UUID of the ATM card to look up
+
+        Returns:
+            str: Balance of account on success
+            bool: False on failure
+        """
+        self._vp('check_balance: Sending request to Bank')
+        pkt = "b" + struct.pack(">36s36s", atm_id, card_id)
+        self.ser.write(pkt)
+
+        while pkt not in "ONE":
+            pkt = self.ser.read()
+        if pkt != "O":
+            return False
+        pkt = self.ser.read(76)
+        aid, cid, bal = struct.unpack(">36s36sI", pkt)
+        self._vp('check_balance: returning balance')
+        return bal
+
+    def withdraw(self, atm_id, card_id, amount):
+        """Requests a withdrawal from the account associated with the card_id
+
+        Args:
+            atm_id (str): UUID of the HSM
+            card_id (str): UUID of the ATM card
+            amount (str): Requested amount to withdraw
+
+        Returns:
+            str: hsm_id on success
+            bool: False on failure
+        """
+        print("bank withdraw1")
+        self._vp('withdraw: Sending request to Bank')
+        print("bank withdraw2")
+
+        command = "w" # withdraw
+        encCommand = eh.aesEncrypt(command, key2) #length = 16
+
+        #len(atm_id) == 72
+        print "len(atm_id):"
+        print len(atm_id)
+        print "len(card_id):"
+        print len(card_id)
+
+        # data = struct.pack(">36s36sI", atm_id, card_id, amount)
+        # encData = eh.aesEncrypt(data, key2)
+
+        encAtmId = eh.aesEncrypt(atm_id, key2)
+        encCardId = eh.aesEncrypt(card_id, key2)
+        encAmount = eh.aesEncrypt(anount, key2)
+
+        print "len(encAtmId):"
+        print len(encAtmId)
+        print "len(encCardId):"
+        print len(encCardId)
+        print "len(encAmount):"
+        print len(encAmount)
+
+        pkt = encCommand + encAtmId + encCardId + encAmount
+
+        # encryption
+        # enc_pkt = eh.aesEncrypt(pkt, key2)
+        # self.ser.write(len(enc_pkt))
+        self.ser.write(pkt)
+        # while pkt not in "ONE":
+        #     pkt = self.ser.read()
+        # if pkt != "O":
+        #     self._vp('withdraw: request denied')
+        #     return False
+        pkt = ''
+        while pkt == '':
+            enc_read_pkt = self.ser.read(72)
+            dec_read_pkt = eh.aesDecrypt(enc_read_pkt, key2)
+            aid, cid = struct.unpack(">36s36s", pkt)
+            self._vp('withdraw: Withdrawal accepted')
+            return True
+
+    def provision_update(self, uuid, pin, balance):
+        pkt = struct.pack(">36s8sI", uuid, pin, balance)
+        self.ser.write("p" + pkt)
