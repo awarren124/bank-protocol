d1c19485d754f5575dff21bda74864eef6eb4cf8
Alexander Warren <arocks124@gmail.com>
2018-08-04 10:44:37 -0400

    Fixed keyhandler stuff

--- protocol/bank/keyDBHandler.py
+++ protocol/bank/keyDBHandler.py
@@ -8,14 +8,18 @@ class KeyDBHandler:
         self.fileName = fn
 
     def writeKey(self, label, key):
-        key = base64.b64encode(key)
+
+        key = base64.b64encode(key).decode('utf-8')
+        print(key)
         with open(self.fileName) as file:
             data = json.load(file)
             data["keys"][label] = key
+            print(data)
         with open(self.fileName, 'w') as file:
-            json.dump(data, file, indent=2)
+            json.dump(data, file, indent=4)
 
     def readKey(self, label):
+        print('HELLO')
         with open(self.fileName) as file:
             data = json.load(file)
             key = data["keys"][label]
